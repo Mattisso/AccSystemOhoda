449bc6c2da1dc73842487db10b7ebf18ad160ce1
Mensah Attisso <mattisso@atd-us.com>
2017-08-21 16:58:30 -0400

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'pm-app',
-    template: `
-        <h1>Angular2: Getting Started</h1>
-    `
+  selector: 'pm-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent {
+  title = 'Angular: Getting Started';
+}
